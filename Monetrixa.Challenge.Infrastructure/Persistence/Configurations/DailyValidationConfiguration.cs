cad45ecbacb78cfc8ad267802892e0a040b61c95
Kouadio Brice Marcel <kbricemarcel@outlook.fr>
2026-04-15 21:26:12 +0000

    feat: configuration of data persistence for the different entities

---
+++ Monetrixa.Challenge.Infrastructure/Persistence/Configurations/DailyValidationConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Monetrixa.ChallengeApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monetrixa.ChallengeApp.Infrastructure.Persistence.Configurations
+{
+    public class DailyValidationConfiguration : IEntityTypeConfiguration<DailyValidation>
+    {
+        public void Configure(EntityTypeBuilder<DailyValidation> builder)
+        {
+            builder.ToTable("DailyValidation");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Status)
+                .IsRequired();
+
+            builder.Property(x => x.Note)
+                .HasMaxLength(1000);
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.DailyValidations)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.ChallengeDay)
+                .WithMany(x => x.DailyValidations)
+                .HasForeignKey(x => x.ChallengeDayId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(x => x.PublishedContent)
+                .WithMany(x => x.DailyValidations)
+                .HasForeignKey(x => x.PublishedContentId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            builder.HasIndex(x => new { x.UserId, x.ChallengeDayId })
+                .IsUnique();
+        }
+    }
+}
---
+++ Monetrixa.Challenge.Infrastructure/Persistence/Configurations/UserChallengeConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Monetrixa.ChallengeApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monetrixa.ChallengeApp.Infrastructure.Persistence.Configurations
+{
+    public class UserChallengeConfiguration : IEntityTypeConfiguration<UserChallenge>
+    {
+        public void Configure(EntityTypeBuilder<UserChallenge> builder)
+        {
+            builder.ToTable("UserChallenge");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.JoinedAtUtc)
+                .IsRequired();
+
+            builder.Property(x => x.Score)
+                .IsRequired();
+
+            builder.HasOne(x => x.User)
+                .WithMany(x => x.UserChallenges)
+                .HasForeignKey(x => x.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(x => x.Challenge)
+                .WithMany(x => x.UserChallenges)
+                .HasForeignKey(x => x.ChallengeId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(x => new { x.UserId, x.ChallengeId })
+                .IsUnique();
+        }
+    }
+}
