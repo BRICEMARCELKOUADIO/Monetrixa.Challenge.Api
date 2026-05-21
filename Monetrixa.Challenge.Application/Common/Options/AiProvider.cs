a87131059df013924938732524611ba817caf455
Kouadio Brice Marcel <kbricemarcel@outlook.fr>
2026-05-21 03:27:41 +0000

    feat: add idea generation module with provider abstraction and OpenAI support

---
+++ Monetrixa.Challenge.Application/Common/Options/AiProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Monetrixa.ChallengeApp.Application.Common.Options
+{
+    public enum AiProvider
+    {
+        OpenAI = 1,
+        Gemini = 2,
+        Mistral = 3,
+        Anthropic = 4
+    }
+}
---
+++ Monetrixa.Challenge.Application/DTOs/Ideas/GenerateIdeasRequest.cs
@@ -0,0 +1,12 @@
+﻿using Monetrixa.ChallengeApp.Domain.Enums;
+
+namespace Monetrixa.ChallengeApp.Application.DTOs.Ideas
+{
+    public class GenerateIdeasRequest
+    {
+        public Guid ChallengeDayId { get; set; }
+        public string Topic { get; set; } = string.Empty;
+        public MoodType Mood { get; set; }
+        public PlatformType Platform { get; set; }
+    }
+}
